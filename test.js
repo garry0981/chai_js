70a4ee47709ff528f4177c083f5a8eab30e8ea4d
garry0981 <gauravgarry6@gmail.com>
2024-12-10 04:30:10 +0000

    basic of javascript

---
+++ test.js
@@ -0,0 +1,19 @@
+let score ="300"
+
+// console.log(typeof(score))
+
+console.log(2+2);
+console.log(2>=2)
+
+// datatype (primitive): string,number,boolen,undefind,null,symbol,bigint
+
+// (non primitive):array,object,function
+
+const id = Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id ===anotherId);
+
+let outSideTem =null
+
+console.log(typeof(outSideTem))
